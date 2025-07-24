ad3d8752941ce1e93cb5299c7f48aa83c4bfd918
Amine Sekkat <121364946+Tedsuno@users.noreply.github.com>
2025-07-25 01:47:42 +0200

    Update producer_traffic.py

---
+++ trafic_routiers/producer_traffic.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import json
+import time
+from kafka import KafkaProducer
+
+# Lire les données CSV
+df = pd.read_csv("merged_activity.csv")
+
+# Kafka Producer config
+producer = KafkaProducer(
+    bootstrap_servers='localhost:9092',
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+# Envoi ligne par ligne dans le topic "activity_data"
+for _, row in df.iterrows():
+    message = {
+        "activity_code": row["Activity"],
+        "start": row["Start"],
+        "end": row["End"],
+        "day": row["Day"],
+        "user_id": row["user_id"]
+    }
+    print("📤 Envoi :", message)
+    producer.send("activity_data", value=message)
+    time.sleep(1)  # ajuste si besoin
+
+producer.flush()
+producer.close()
+print("✅ Fin de l’envoi Kafka")
